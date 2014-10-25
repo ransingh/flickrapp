d6b7c89b9e9b39f85aea50f97f445ad4d95067c2
ransingh <ranbir.singh@iplayup.com>
2014-10-25 12:02:11 +1100

    [FirstCommit] Created flickr app repo & added gems
    1. Created Rails 4 based flickr app
    2. Added flickraw gem for using  Flickr API
    3. Using bootstrap for the UI

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flickrapp::Application.config.secret_key_base = '9d80c754828ae6365e416f5e5e62b53af9cd5b5ce127744c6246992e10cbdbaade008945255a603fc5a7d13caac36df0f79fa6dada051a26008b227760b45a7f'
